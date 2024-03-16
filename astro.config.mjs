e1ad23cea72cfeea55b598c527ead0d77531da50
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-03-16 17:34:40 +0100

    github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://itsvalyria.github.io',
+    base: '/we-love-web-blog',
+});
