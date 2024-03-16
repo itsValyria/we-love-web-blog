c0fc45dc754a6ae8e4880494c43165e866d71896
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-03-16 17:55:22 +0100

    astro config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://itsvalyria.github.io',
-    base: '/we-love-web-blog',
+    site: 'https://blog.eefhaller.com',
 });
